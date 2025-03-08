2bdeeb0eeddbe86ab4701003b2bfc8274a2c7455
yjy <3253932102@qq.com>
2025-03-08 15:32:58 +0800

    ⚡️ Perf: add light

--- main/main.c
+++ main/main.c
@@ -3,16 +3,20 @@
 #include "freertos/task.h"
 #include "nvs_flash.h"
 #include "uart_task.h"
+#include "ws2812_control.h"
 
 QueueHandle_t joint_queue;
 
 void app_main() {
 
-  joint_queue = xQueueCreate(20, 39);
+  joint_queue = xQueueCreate(30, 39);
 
   nvs_flash_init();
   uart_init();
   bluetooch_init();
+
+  ws2812_strip_t *WS2812 = ws2812_create();
+  led_set_on(WS2812, COLOR_BLUE);
 
   xTaskCreate(tx_task, "tx_task", 2048, joint_queue, 5, NULL);
 }
